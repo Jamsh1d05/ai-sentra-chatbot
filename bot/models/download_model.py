bfd23782c600ccaccf1399f3c894e248d1e800ff
Jamshid <116630243+Jamsh1d05@users.noreply.github.com>
2025-11-24 11:07:36 +0500

    Fix : formatting  download_model.py

--- bot/models/download_model.py
+++ bot/models/download_model.py
@@ -2,8 +2,9 @@ from transformers import BartForConditionalGeneration, BartTokenizer
 
 model_name = "sshleifer/distilbart-cnn-12-6"
 
-print("⏬ Downloading model and tokenizer...")
+print("Downloading model and tokenizer...")
 model = BartForConditionalGeneration.from_pretrained(model_name)
 tokenizer = BartTokenizer.from_pretrained(model_name)
 
-print("✅ Model and tokenizer downloaded successfully.")
+print("Model and tokenizer downloaded successfully.")
+
